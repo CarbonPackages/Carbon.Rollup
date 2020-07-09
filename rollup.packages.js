86d209b4c6227628fa21d64623a024f7b13646e9
Samuel Hauser <samuel.hauser@gmail.com>
2020-07-09 08:25:03 +0200

    configure input folder for each package

--- rollup.packages.js
+++ rollup.packages.js
@@ -2,7 +2,8 @@
  * Pass an object with the following properties to the array:
  *
  * packageName   <string>  (required) The name of the package
- * filenames     <array>   (required) The names of the entry files
+ * filenames      <array>   (required) The names of the entry files
+ * folder        <string>  (optional) (default: `Fusion`) The folder where to look for the entry files
  * inline        <boolean> (optional) (default: `false`) Flag to toggle if the files should be written to `Resources/Private/Templates/InlineAssets`
  * sourcemap     <boolean> (optional) (default: `true`) Flag to toggle source map generation
  * format        <string>  (optional) (default: `iife`)
