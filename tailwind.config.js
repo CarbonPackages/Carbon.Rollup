a37183b00d38363b12ca4c94987b24eb6e205e57
Jon Uhlmann <me@jonnitto.ch>
2020-07-15 14:40:38 +0200

    🎨 Prettier tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,28 @@
 // tailwind.config.js
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
     theme: {
         extend: {
-            maxWidth: theme => ({
-                'screen-md-p': `calc(${theme('screens.md')} + ${theme('spacing.8')})`
+            maxWidth: (theme) => ({
+                "screen-md-p": `calc(${theme("screens.md")} + ${theme("spacing.8")})`,
             }),
             fontFamily: {
-                sans: ['HeroNew', ...defaultTheme.fontFamily.sans]
+                sans: ["HeroNew", ...defaultTheme.fontFamily.sans],
             },
-            padding: theme => ({
-                ...theme('spacing'),
-                '1/1': '100%',
-                '768/525': 100 / (768 / 525) + '%'
+            padding: (theme) => ({
+                ...theme("spacing"),
+                "1/1": "100%",
+                "768/525": 100 / (768 / 525) + "%",
             }),
             listStyleType: {
-                square: 'square'
-            }
-        }
+                square: "square",
+            },
+        },
     },
     purge: [
-        './DistributionPackages/**/*.fusion',
-        './DistributionPackages/**/*.html',
-        './Data/Temporary/Development/MonocleViews/**/*.html'
-    ]
+        "./DistributionPackages/**/*.fusion",
+        "./DistributionPackages/**/*.html",
+        "./Data/Temporary/Development/MonocleViews/**/*.html",
+    ],
 };
