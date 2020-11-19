0388f372ba3f8d394a05bbd2539285b6e7a54cd7
Jon Uhlmann <me@jonnitto.ch>
2020-11-19 15:44:09 +0100

    :boom: Switch to almost default tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,51 +1,15 @@
-// tailwind.config.js
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 module.exports = {
+    purge: [
+        "./DistributionPackages/**/*.fusion",
+        "./DistributionPackages/**/*.html",
+        "./Data/Temporary/Development/MonocleViews/**/*.html",
+    ],
+    darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {
-            screens: {
-                light: { raw: "(prefers-color-scheme: light)" },
-                dark: { raw: "(prefers-color-scheme: dark)" },
-            },
-            maxWidth: (theme) => ({
-                "screen-md-p": `calc(${theme("screens.md")} + ${theme("spacing.8")})`,
-            }),
-            fontFamily: {
-                sans: ["HeroNew", ...defaultTheme.fontFamily.sans],
-            },
-            padding: (theme) => ({
-                ...theme("spacing"),
-                "1/1": "100%",
-                "768/525": 100 / (768 / 525) + "%",
-            }),
-            listStyleType: {
-                square: "square",
-            },
-        },
+        extend: {},
     },
-    dark: "media", // or 'class'
-    experimental: {
-        applyComplexClasses: true,
-        uniformColorPalette: true,
-        extendedSpacingScale: true,
-        defaultLineHeights: true,
-        extendedFontSizeScale: true,
-        standardFontWeights: true,
-        // darkModeVariant: true,
+    variants: {
+        extend: {},
     },
-    future: {
-        removeDeprecatedGapUtilities: true,
-        purgeLayersByDefault: true,
-    },
-    purge: {
-        mode: "layers",
-        layers: ["base", "components", "utilities"],
-        preserveHtmlElements: true,
-        content: [
-            "./DistributionPackages/**/*.fusion",
-            "./DistributionPackages/**/*.html",
-            "./Data/Temporary/Development/MonocleViews/**/*.html",
-        ],
-    },
+    plugins: [],
 };
